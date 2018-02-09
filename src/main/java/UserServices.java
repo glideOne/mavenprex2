b35bbd3e67f6faffcbcced033335b18462f5c760
Ovidiu Zakarias <ovidiu.zakarias@pitechnologies.ro>
2018-02-09 17:03:35 +0200

    added

---
+++ src/main/java/UserServices.java
@@ -0,0 +1,22 @@
+/**
+ * Created by ozakarias on 1/9/2018.
+ */
+public class UserServices {
+
+    public void getAllUsers() {
+        System.out.println("Get all users method called.");
+    }
+
+    public void addUser() {
+        System.out.println("Add user method was called.");
+    }
+
+    //2.e
+    public static void doSomethingStatic() {
+        System.out.println("Something was done. Statically.");
+    }
+
+    //2.e
+    public static void doSomethingElse() { System.out.println("Something else was done. Statically."); }
+
+}
